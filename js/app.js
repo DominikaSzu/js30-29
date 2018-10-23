dac138c86c0c0f51f8aaa52586d9e160210ea02c
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-23 12:20:49 +0200

    Counting down & changing from sec to min

--- js/app.js
+++ js/app.js
@@ -3,14 +3,20 @@ let interval;
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
+    displaySecondsLeft(seconds);
     
     interval = setInterval(()=> {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         if (secondsLeft < 0) {
             clearInterval(interval);
             return;
-        }
-        
-        console.log(secondsLeft)
-    }, 1000)
+        }    
+      displaySecondsLeft(secondsLeft);
+    }, 1000);
+}
+
+function displaySecondsLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    console.log(`${minutes} : ${secondsLeft}`);
 }
